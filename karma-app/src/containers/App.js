fc60e9d88e4f1388013b09c7d027f2fac3612036
Florian Jost <florian.jost89@gmail.com>
2019-07-25 17:17:06 +0200

    chore: changed file structure

---
+++ karma-app/src/containers/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App = () => (
+  <View>
+    <Text>Open up App.js to start working on your app!</Text>
+  </View>
+);
+
+export default App;
