1fbcd7aba0b2458c2fc2bd72b8d2c4eb24e06a66
Florian Jost <florian.jost89@gmail.com>
2019-07-29 20:53:56 +0200

    feat: lottie animation

--- karma-app/navigators/index.js
+++ karma-app/navigators/index.js
@@ -2,12 +2,14 @@ import { createSwitchNavigator, createAppContainer } from 'react-navigation';
 import AuthNavigator from './AuthNavigator';
 import AppNavigator from './AppNavigator';
 import AuthLoading from '../screens/AuthLoading';
+import LevelUp from '../screens/LevelUp';
 
 const AppContainer = createAppContainer(createSwitchNavigator(
   {
     AuthLoading,
     AuthNavigator,
     AppNavigator,
+    LevelUp
   },
   {
     initialRouteName: 'AuthLoading',
--- karma-app/screens/LevelUp.js
+++ karma-app/screens/LevelUp.js
@@ -1,6 +1,8 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Text, Image, View, StyleSheet, Button } from 'react-native';
+import LottieView from 'lottie-react-native'
 const logo = require('../assets/karma-logo.png');
+const animationData = require('../assets/lottie-files/2056-gagaha.json');
 
 
 
@@ -8,23 +10,28 @@ const LevelUp = (props) => {
   return (
     <View style={styles.container}>
       <View style={styles.topContainer}>
-        <Text h1 style={styles.textUp}>WELL DONE</Text>
-        <Image
-          style={styles.image}
-          source={logo}
-        />
+        <Text h1 style={styles.textUp}>YOU ROCK</Text>
+        <View style={styles.lottie}>
+          <LottieView
+            source={animationData}
+            autoPlay
+            loop
+          />
+        </View>
         <Text h2 style={styles.textDown}>Your Karma improved to the next level!</Text>
-        <Button
-          style={styles.button}
-          onPress={() => props.navigation.navigate('Main')}
-          title='Continue Your Path'
-          color='#cc2e5d'
-          accessibilityLabel='Click me to go back'
-        />
+        <View style={styles.button}>
+          <Button
+            onPress={() => props.navigation.navigate('Main')}
+            title='CONTINUE YOUR PATH'
+            color='#cc2e5d'
+            accessibilityLabel='Click me to go back'
+          />
+        </View>
       </View>
     </View>
 
   );
+
 };
 
 const styles = StyleSheet.create({
@@ -46,12 +53,25 @@ const styles = StyleSheet.create({
     margin: 20,
     fontSize: 40,
     fontWeight: '600',
+    color: '#cc2e5d'
   },
   textDown: {
     margin: 20,
     fontSize: 15,
   },
+  lottie: {
+    width: 800,
+    height: 200,
+  },
+  button: {
+    marginTop: 80,
+  }
 
 });
 
 export default LevelUp;
+
+/*  <Image
+         style={styles.image}
+         source={logo}
+       /> */
