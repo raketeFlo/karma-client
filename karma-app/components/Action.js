c7581789bd013fee47c2cfacce9d278255d8cde2
Florian Jost <florian.jost89@gmail.com>
2019-07-29 12:34:21 +0200

    feat: conditional rendering icons

--- karma-app/components/Action.js
+++ karma-app/components/Action.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import {
-  TouchableOpacity, StyleSheet
+  TouchableOpacity, StyleSheet, Image
 } from 'react-native';
 import { Card, Paragraph } from 'react-native-paper';
+const people = require('../assets/Icons/b2b.png');
+const volunteer = require('../assets/Icons/charity.png');
+const transport = require('../assets/Icons/bus.png');
+const environment = require('../assets/Icons/planet-earth.png');
+const relationship = require('../assets/Icons/friends.png');
 
 
 const Action = ({ actions, addExp }) => {
+  // conditional rendering of icons
+  const icons = (category) => {
+    if (category === 'People') {
+      return people;
+    } else if (category === 'Relationship') {
+      return relationship;
+    } else if (category === 'Transports') {
+      return transport;
+    } else if (category === 'Volunteering') {
+      return volunteer;
+    } else if (category === 'Environment') {
+      return environment;
+    }
+  };
 
   return (
     <TouchableOpacity onPress={() => addExp(actions.exp_points)}>
       <Card style={styles.container}>
         <Card.Content style={styles.cardContainer}>
-          <Paragraph>{actions.category}</Paragraph>
+          <Paragraph><Image source={icons(actions.category)} style={styles.symbols} /></Paragraph>
           <Paragraph>{actions.description}</Paragraph>
           <Paragraph>{actions.exp_points}</Paragraph>
         </Card.Content>
@@ -31,6 +50,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  symbols: {
+    padding: 20,
+    height: 20,
+    width: 30,
+  }
 });
 
 export default Action;
